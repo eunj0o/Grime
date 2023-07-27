e6a3903cccf8f937c31994ae0d518836114ec04b
easyone824 <98134278+easyone824@users.noreply.github.com>
2023-07-27 15:44:20 +0900

    paintingdiary.xml
    일기장 화면 + 폰트 추가

---
+++ app/src/main/java/com/example/grime/PaintingDiary.kt
@@ -0,0 +1,11 @@
+package com.example.grime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaintingDiary : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_painting_diary)
+    }
+}
