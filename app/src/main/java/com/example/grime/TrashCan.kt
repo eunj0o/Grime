1f9fe6f701992262ec9fce9121880e9a68085ef0
easyone824 <98134278+easyone824@users.noreply.github.com>
2023-07-28 23:32:21 +0900

    setting.xml2
    세팅화면 + 휴지통, 폰트 변경, 배경색 변경 / 폰트 추가

---
+++ app/src/main/java/com/example/grime/TrashCan.kt
@@ -0,0 +1,11 @@
+package com.example.grime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrashCan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trash_can)
+    }
+}
