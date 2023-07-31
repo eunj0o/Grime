eb0c1bf7a33be43fe6b8e9beff192291d6267f6f
eunj0o <eun2001@swu.ac.kr>
2023-07-31 21:14:18 +0900

    fix: add default background color

--- app/src/main/java/com/example/grime/ThemeUtil.kt
+++ app/src/main/java/com/example/grime/ThemeUtil.kt
@@ -3,6 +3,7 @@ package com.example.grime
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Resources
+import android.graphics.Color
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -22,6 +23,8 @@ class ThemeUtil {
                 val layoutColor = sharedPreferences.getInt("layoutColor", 0)
 
                 layout.setBackgroundColor(layoutColor)
+            } else {
+                layout.setBackgroundColor(Color.parseColor("#DDF0F6"))
             }
         }
     }
